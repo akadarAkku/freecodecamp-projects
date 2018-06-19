f9c22865cfefda47173ce96ee9d568e8f1f30a1f
jm18457 <jm18457@gmail.com>
2018-06-19 17:38:59 +0200

    Finished project, but there is bug / problem.
    The FCC tests do not work, due to again browser issues.
    The tests pass when using codepen.
    The linked project will have codepen link, because it is working.

--- front-end-libraries/markdown-previewer/src/App.js
+++ front-end-libraries/markdown-previewer/src/App.js
@@ -1,7 +1,43 @@
 import React, { Component } from "react";
+import marked from "marked";
+import "bulma/css/bulma.css";
+import "./App.css";
+import { preloadedTextarea } from "./constants";
 
 export default class App extends Component {
+  state = {
+    textarea: preloadedTextarea
+  };
+
+  getMarkdownText = () => {
+    const rawMarkup = marked(this.state.textarea, { breaks: true });
+    return { __html: rawMarkup };
+  };
+
   render() {
-    return <div>nothing</div>;
+    return (
+      <main className="main">
+        <section className="textarea-wrapper">
+          <form>
+            <textarea
+              id="editor"
+              rows="15"
+              className="textarea"
+              placeholder="e.g. Hello world"
+              value={this.state.textarea}
+              onChange={e => this.setState({ textarea: e.target.value })}
+            />
+          </form>
+        </section>
+        <section className="content-wrapper">
+          <div className="content">
+            <div
+              id="preview"
+              dangerouslySetInnerHTML={this.getMarkdownText()}
+            />
+          </div>
+        </section>
+      </main>
+    );
   }
 }
