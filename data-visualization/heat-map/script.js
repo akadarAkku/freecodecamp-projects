6791844e8e5e575665882a26a3b7a2ffc9313dd4
jm18457 <jm18457@gmail.com>
2018-06-24 19:38:47 +0200

    Create boilerplate to make graph responsive

--- data-visualization/heat-map/script.js
+++ data-visualization/heat-map/script.js
@@ -4,4 +4,50 @@ const getData = () => {
   return fetch(URL).then(response => response.json());
 };
 
-getData().then(data => console.log(data));
+getData().then(data => {
+  drawGraph(data);
+});
+
+const drawGraph = data => {
+  const margin = { top: 0, right: 0, bottom: 0, left: 0 };
+  const fullWidth = 1920;
+  const fullHeight = 1080;
+  const width = fullWidth - margin.left - margin.right;
+  const height = fullHeight - margin.top - margin.bottom;
+
+  const svg = d3
+    .select("#chart")
+    .append("svg")
+    .attr("width", fullWidth)
+    .attr("height", fullHeight)
+    .call(responsivefy)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  svg
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height)
+    .style("fill", "lightblue")
+    .style("stroke", "green");
+};
+
+const responsivefy = svg => {
+  const container = d3.select(svg.node().parentNode);
+  const width = parseInt(svg.style("width"));
+  const height = parseInt(svg.style("height"));
+  const aspect = width / height;
+
+  const resize = () => {
+    const targetWidth = parseInt(container.style("width"));
+    svg.attr("width", targetWidth);
+    svg.attr("height", Math.round(targetWidth / aspect));
+  };
+
+  svg
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .attr("preserveAspectRatio", "xMinYMid")
+    .call(resize);
+
+  d3.select(window).on(`resize.${container.attr("id")}`, resize);
+};
